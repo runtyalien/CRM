ce1a03b22f9c1f7f0b29b6fa015e3c9801df0865
runtyalien <omgadkar@gmail.com>
2024-06-29 01:19:53 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import { useNotificationProvider } from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import { dataProvider, liveProvider } from "./providers/data";
+import routerBindings, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import { App as AntdApp } from "antd";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+          <AntdApp>
+            <DevtoolsProvider>
+              <Refine
+                dataProvider={dataProvider}
+                liveProvider={liveProvider}
+                notificationProvider={useNotificationProvider}
+                routerProvider={routerBindings}
+                options={{
+                  syncWithLocation: true,
+                  warnWhenUnsavedChanges: true,
+                  useNewQueryKeys: true,
+                  projectId: "Qq03RV-WRmJ03-wAkMps",
+                  liveMode: "auto",
+                }}
+              >
+                <Routes>
+                  <Route index element={<WelcomePage />} />
+                </Routes>
+                <RefineKbar />
+                <UnsavedChangesNotifier />
+                <DocumentTitleHandler />
+              </Refine>
+              <DevtoolsPanel />
+            </DevtoolsProvider>
+          </AntdApp>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/providers/auth.ts
@@ -0,0 +1,11 @@
+import { AuthBindings } from "@refinedev/core";
+import { API_URL, dataProvider } from "./data";
+
+export const authCredentials = {
+    email: "testing@test.com",
+    password: "testtest",
+};
+
+// export const authProvider: AuthBindings = {
+//     login: async({ email }) => {}
+// }
